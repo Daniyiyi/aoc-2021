acb5345b14290d497089fb50da6e61278d9abd94
Daniyiyi <danial.amirjalali5@gmail.com>
2021-12-08 00:53:21 +0100

    day 2 finish

---
+++ day_02_2/src/main.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let mut _test: Vec<String>;
+    let mut horizontal = 0;
+    let mut death = 0;
+    let mut aim = 0;
+    // let mut vec = Vec::new();
+
+    //read file line by line
+    if let Ok(lines) = read_lines("input.txt") {
+        for line in lines {
+            if let Ok(ip) = line {
+                //convert string to int and pushes it to vector
+                _test = ip.split_whitespace().map(|s| s.to_string()).collect();
+                let tmp = _test[1].parse::<i32>().unwrap();
+                if _test[0] == "forward" {
+                    horizontal += tmp;
+                    death += tmp * aim;
+                } else if _test[0] == "up" {
+                    aim -= tmp;
+                } else {
+                    aim += tmp;
+                }
+            }
+        }
+    }
+    println!("{}", horizontal * death);
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
